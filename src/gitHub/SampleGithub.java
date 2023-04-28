021352c25b4d4e7f93361c0d4015ffa569884575
Sriharish657 <sriharish657@gmail.com>
2023-04-28 18:55:57 +0530

    Sri's task

---
+++ src/gitHub/SampleGithub.java
@@ -0,0 +1,15 @@
+package gitHub;
+
+public class SampleGithub {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		
+	}
+
+}
